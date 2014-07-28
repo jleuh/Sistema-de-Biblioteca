195624b20eb32d148781c019f4ba7587e56ed4bf
Dailson <daylsongomes@gmail.com>
2014-07-28 16:39:28 -0300

    Quinto commit: Colocando os Getters e Setters na classe Texto

--- src/br/ifrn/tads/acervo/Texto.java
+++ src/br/ifrn/tads/acervo/Texto.java
@@ -2,4 +2,15 @@ package br.ifrn.tads.acervo;
 
 public class Texto {
 
+		private String autor;
+
+		public String getAutor() {
+			return autor;
+		}
+
+		public void setAutor(String autor) {
+			this.autor = autor;
+		}
+		
+		
 }
