9e998d86f2ac3170ed9e4bdc16381604832e281c
Dailson <daylsongomes@gmail.com>
2014-07-28 16:38:28 -0300

    Quarto commit: Colocando os Getters e Setters na classe Apostila

--- src/br/ifrn/tads/acervo/Apostila.java
+++ src/br/ifrn/tads/acervo/Apostila.java
@@ -2,6 +2,29 @@ package br.ifrn.tads.acervo;
 
 public class Apostila {
 	
+	private String titulo;
+	private String autor;
+	private int quantidade;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
 	
 	
 }
